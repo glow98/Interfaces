b75d5758e5851b80943054230b9df55f29a67af0
glow98 <40080461+glow98@users.noreply.github.com>
2019-08-22 21:42:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ave Paloma = new Ave("Paloma");
+            Ave Cotorro = new Ave("Cotorro");
+            Program main = new Program();
+            main.imprimir(Paloma);
+            main.imprimir(Cotorro);
+            Console.ReadKey();
+        }
+        public void imprimir(Ave ave)
+        {
+            Console.WriteLine("Soy un {0} tengo {1} Pata(s), {2} Ala(s) y {3} Cola(s)", ave.Nombre, ave.Patas, ave.Alas, ave.Cola);
+            Console.WriteLine("Puedo");
+            ave.Volar();
+            Console.WriteLine("Y tambien puedo");
+            ave.Comer();
+        }
+    }
+}
